65132f0fee83926647b9628ebe6061a5fa411796
ildevblu <mtt.maran@gmail.com>
2023-11-13 17:02:33 +0100

    0.3.1 - use count api for background pr check

--- js/engine/background.js
+++ js/engine/background.js
@@ -6,17 +6,10 @@ const updatePRBadgeCount = async () => {
 
 		if(!storage.bbUrl) throw "bitBucket Url not set";
 
-		fetch(`${storage.bbUrl}/rest/api/latest/inbox/pull-requests?role=REVIEWER&start=0&limit=10&avatarSize=64&withAttributes=true&state=OPEN&order=oldest`)
-		.then((response) => response.json())
-		.then((json) => {
-			if(json.size) {
-				// Update badge color and count
-				chrome.action.setBadgeBackgroundColor({ color: "#FF2020" }); // Imposta il colore di sfondo trasparente
-				chrome.action.setBadgeText({ text: "" + json.size });
-			} else {
-				chrome.action.setBadgeText({text: ""});
-			}
-		}); 
+		const prCount = await(await fetch(`${storage.bbUrl}/rest/api/latest/inbox/pull-requests/count`)).json();
+		
+		chrome.action.setBadgeBackgroundColor({ color: "#FF2020" });
+		chrome.action.setBadgeText({ text: prCount.count ? "" + prCount.count : "" });
 	} catch (e) {
 		chrome.action.setBadgeText({text: ""});
 		console.log(e);
