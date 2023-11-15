bd54204541465fdd9bb70c9e88f852e088b306bd
ildevblu <mtt.maran@gmail.com>
2023-11-15 09:55:52 +0100

    Total style rework + avatars

--- js/app/popup.js
+++ js/app/popup.js
@@ -16,20 +16,30 @@ document.addEventListener("DOMContentLoaded", async () => {
   Object.assign(storageCache, storage);
   // step 3: if setup ok > show list, else show setup
   if (storageCache.bbUrl) {
-    fetch(
-      `${storageCache.bbUrl}/rest/api/latest/inbox/pull-requests?role=REVIEWER&start=0&limit=10&avatarSize=64&withAttributes=true&state=OPEN&order=oldest`
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        updatePRBadgeCount(json.size);
-        showPRs(json.values);
+    const prs = fetch(`${storageCache.bbUrl}/rest/api/latest/inbox/pull-requests?role=REVIEWER&start=0&limit=10&avatarSize=64&withAttributes=true&state=OPEN&order=oldest`).then(response => response.json());;
+    const recent = fetch(`${storageCache.bbUrl}/rest/awesome-graphs/latest/people/recent`).then(response => response.json());
+    
+    Promise.all([prs, recent])
+      .then(async ([prs, recent]) => {
+        await updateSelfProfile(recent);
+        updatePRBadgeCount(prs.size);
+        showPRs(prs.values);
         prSection.removeAttribute("hidden");
+      })
+      .catch(error => {
+        console.error(error);
       });
+
   } else {
     updatePRBadgeCount(0);
     showSetupSection();
   }
 });
+
+const updateSelfProfile = async profile => {
+  storageCache.email = profile[0]?.meta[0];
+  await chrome.storage.sync.set(storageCache);
+}
 
 const showSetupSection = () => {
   if(storageCache.lastBbUrl) {
@@ -87,9 +97,66 @@ const updatePRBadgeCount = (count) => {
   }
 };
 
+const showReviewers = pr => {
+  let htmlOutput = "";
+
+  //find ourselves
+
+  const selfProfile = pr.reviewers.find(reviewer => reviewer.user.emailAddress === storageCache.email);
+  let remainingReviewers = pr.reviewers.filter(reviewer => reviewer.user.emailAddress !== storageCache.email);
+  const unapprovedReviewers = remainingReviewers.filter(reviewer => reviewer.status === "UNAPPROVED");
+  const approvedReviewers = remainingReviewers.filter(reviewer => reviewer.status === "APPROVED");
+  const needWorkReviewers = remainingReviewers.filter(reviewer => reviewer.status !== "APPROVED" && reviewer.status !== "UNAPPROVED" );
+  
+  let needWorkReviewersToShow = 0;
+  let approvedReviewersToShow = 0;
+  let unapprovedReviewersToShow = 2;
+
+  if(needWorkReviewers.length > 0) {
+    unapprovedReviewersToShow--;
+    needWorkReviewersToShow++;
+  }
+
+  if(approvedReviewers.length > 0) {
+    unapprovedReviewersToShow--;
+    approvedReviewersToShow++;
+  }
+
+  if(pr.reviewers.length > 3) {
+    htmlOutput += `<span class="avatar reviewer others">${pr.reviewers.length - 3}</span>`;
+  }
+
+  for(let i = 0; i < approvedReviewersToShow && i < approvedReviewers.length; i++) {
+    htmlOutput += `<span class="badge"><img class="badgeIcon" src="../../img/check.svg"></span>`;
+    htmlOutput += `<img class="avatar reviewer" src=${storageCache.bbUrl}${approvedReviewers[i].user.avatarUrl}>`;
+  }
+
+  for(let i = 0; i < unapprovedReviewersToShow && i < unapprovedReviewers.length; i++) {
+    htmlOutput += `<img class="avatar reviewer" src=${storageCache.bbUrl}${unapprovedReviewers[i].user.avatarUrl}>`;
+  }
+
+  for(let i = 0; i < needWorkReviewersToShow && i < needWorkReviewersToShow.length; i++) {
+    htmlOutput += `<img class="avatar reviewer" src=${storageCache.bbUrl}${needWorkReviewers[i].user.avatarUrl}>`;
+  }
+
+  htmlOutput += `<img class="avatar reviewer" src=${storageCache.bbUrl}${selfProfile.user?.avatarUrl}>`;
+
+  return htmlOutput;
+}
+
 const showPRs = (prList) => {
   prListUL.innerHTML = "";
-  prList.forEach((pr) => {
-    prListUL.innerHTML += `<li><a target="_blank" href="${pr.links.self[0]?.href}">${pr.title}</a><div>${pr.description}</div></li>`;
+  prList.forEach(pr => {
+    prListUL.innerHTML += `
+    <li class="pr">
+      <img class="avatar author" src=${storageCache.bbUrl}${pr.author.user.avatarUrl}>
+      <div class="prLinkContainer">
+        <a class="prLink" target="_blank" href="${pr.links.self[0]?.href}">${pr.title}</a>
+        <div class="repo">${pr.fromRef.repository.project.name} / ${pr.fromRef.repository.name}</div>
+      </div>
+      <div class="reviewers">
+        ${ showReviewers(pr) }
+      </div>
+    </li>`;
   });
 };
